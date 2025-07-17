4f91d3cbf140a61c9fcaf948e26abdcae8bb55a8
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-07-17 23:08:03 +0530

    ..

--- frontend/src/Navbar/navbar.jsx
+++ frontend/src/Navbar/navbar.jsx
@@ -53,9 +53,12 @@ function Navbar() {
           >
             Contact Us
           </a>
-          <div className="p-[2px] rounded-xl bg-gradient-to-r from-cyan-400 via-purple-500 to-pink-500">
-            <button className="bg-[#110E19] text-white px-5 py-2 rounded-[10px] uppercase text-sm font-medium tracking-wider hover:bg-opacity-90 transition-all">
-              Get a Quote
+          <div className="px-2 pt-2">
+            <button className="relative w-full inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-2 text-sm font-medium text-white backdrop-blur-3xl">
+                  Get a Quote
+              </span>
             </button>
           </div>
         </div>
