3092850a2cf08dcae0da14d8fef635c21d413304
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-07-17 11:31:02 +0530

    ..

--- frontend/src/Navbar/navbar.jsx
+++ frontend/src/Navbar/navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
     return (
         <nav className="bg-[#110E19] py-6">
-        <div className="container mx-auto flex justify-between items-center px-6">
+        <div className="container mx-auto flex justify-between items-center">
             <div className="flex items-center space-x-4">
             <a href="/" aria-label="Home">
                 <img src="" alt="" />
@@ -30,9 +31,7 @@ function Navbar() {
             
             <div className="md:hidden">
             <button className="text-white focus:outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-                </svg>
+                <GiHamburgerMenu />
             </button>
             </div>
             
