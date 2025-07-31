cdc4d35219cf3afdac5aa7748e8eaaf5228b5fa4
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2025-07-31 22:07:20 +0530

    ..

--- frontend/src/Navbar/navbar.jsx
+++ frontend/src/Navbar/navbar.jsx
@@ -110,7 +110,7 @@ function Navbar({ darkMode, setDarkMode }) {
             <button className="relative w-full inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full items-center justify-center rounded-full bg-slate-950/80 dark:bg-gray-700/80 px-6 py-2 text-sm font-medium text-white backdrop-blur-3xl">
-                Get a Quote
+                Verify Certificate
               </span>
             </button>
           </motion.div>
